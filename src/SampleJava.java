927f24615957e9d6e9f4a1705a9fb8cd59f4946c
BT_4N2_13 <yhuang6817@bths.edu>
2022-09-20 11:43:10 -0400

    Added print statement for movie

--- src/SampleJava.java
+++ src/SampleJava.java
@@ -1,5 +1,6 @@
 public class SampleJava {
     public static void main(String[] args) {
         System.out.println("I'm stupid");
+        System.out.println("I don't have a favorite movie");
     }
 }
