9b8526f09ca01fa5541d1ca5fb5314aae2fa5cfb
YunHuang123 <114007330+YunHuang123@users.noreply.github.com>
2022-09-20 17:49:16 -0400

    Added my favorite food through a print statement.

--- src/SampleJava.java
+++ src/SampleJava.java
@@ -1,6 +1,7 @@
 public class SampleJava {
     public static void main(String[] args) {
-        System.out.println("I'm stupid");
+        System.out.println("Yun");
         System.out.println("I don't have a favorite movie");
+        System.out.println("Ramen");
     }
 }
